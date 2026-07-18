586750c2d2522589be1b293efdbf540703793407
Yoyakuapp <info@yamato.jp>
2026-07-19 00:33:35 +0200

    実際にGeistフォントを読み込むよう修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
+import { Geist } from "next/font/google";
 
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Yoyaku",
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" className="h-full antialiased">
+    <html lang="ja" className={`h-full antialiased ${geistSans.variable}`}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
