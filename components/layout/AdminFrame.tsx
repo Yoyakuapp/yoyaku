42c1acb58d90c04ff5d6646dd635175022edf079
Yoyakuapp <info@yamato.jp>
2026-07-19 19:45:12 +0200

    Add StoreTopPageLink component to AdminFrame

--- components/layout/AdminFrame.tsx
+++ components/layout/AdminFrame.tsx
@@ -1,3 +1,5 @@
+import StoreTopPageLink from "@/components/admin/StoreTopPageLink";
+
 type AdminFrameProps = {
   children: React.ReactNode;
 };
@@ -6,6 +8,7 @@ export default function AdminFrame({ children }: AdminFrameProps) {
   return (
     <main className="min-h-screen bg-stone-100 flex justify-center">
       <div className="w-full max-w-[430px] md:max-w-3xl lg:max-w-5xl min-h-screen bg-stone-100 px-4 py-4 md:px-8 md:py-8">
+        <StoreTopPageLink />
         {children}
       </div>
     </main>
